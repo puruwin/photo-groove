62a84c77b593cb83fb197db429a4d1fd6a350c32
David Perez <49234343z@globalia.com>
2023-12-01 17:15:19 +0100

    inital commit

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,41 @@
+module PhotoGroove exposing (main)                              -- Declara un nuevo modulo
+
+import Html exposing (div, h1, img, text)                       -- Importa otros modulos
+import Html.Attributes exposing (..)
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ] 
+        , div [ id "thumbnails" ] 
+            (List.map 
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url) ]
+        ]
+        []
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+    ]
+    , selectedUrl = "1.jpeg"
+    }
+
+main =
+    view initialModel
