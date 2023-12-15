604c2bd6dd701301cf0a1c30333bbbc51097dffe
puruwin <taborlindp@gmail.com>
2023-12-15 13:21:34 +0100

    event handlers with recursive msgs

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -11,16 +11,51 @@ import Html.Attributes exposing (title)
 import Html.Attributes exposing (size)
 import Dict exposing (Dict)
 
+type Folder =
+  Folder
+    { name : String
+    , expanded : Bool
+    , photoUrls : List String
+    , subfolders : List Folder
+    }
 
 type alias Model =
   { selectedPhotoUrl : Maybe String
   , photos : Dict String Photo
+  , root : Folder
   }
+
+type FolderPath
+  = End
+  | Subfolder Int FolderPath
+
+toggleExpanded : FolderPath -> Folder -> Folder
+toggleExpanded path (Folder folder) =
+  case path of
+    End ->
+      Folder { folder | expanded = not folder.expanded }
+    
+    Subfolder targetIndex remainingPath ->
+      let
+        subfolders : List Folder
+        subfolders =
+          List.indexedMap transform folder.subfolders
+        
+        transform : Int -> Folder -> Folder
+        transform currentIndex currentSubfolder =
+          if currentIndex == targetIndex then
+            toggleExpanded remainingPath currentSubfolder
+          
+          else
+            currentSubfolder
+      in
+      Folder { folder | subfolders = subfolders }
 
 initialModel : Model
 initialModel =
   { selectedPhotoUrl = Nothing
   , photos = Dict.empty
+  , root = Folder { name = "Loading...",  expanded = True, photoUrls = [], subfolders = [] }
   }
 
 init : () -> ( Model, Cmd Msg )
@@ -60,15 +95,50 @@ modelDecoder =
           }
         )
       ]
+    , root =
+        Folder
+        { name = "Photos", expanded = True, photoUrls = []
+        , subfolders =
+          [ Folder 
+            { name = "2016", expanded = True, photoUrls = [ "trevi", "coli" ]
+            , subfolders =
+              [ Folder
+                { name = "outdoors"
+                , expanded = True, photoUrls = [], subfolders = []
+                }
+              , Folder
+                { name = "indoors"
+                , expanded = True, photoUrls = [ "fresco" ], subfolders = []
+                }
+              ] 
+            }
+          , Folder
+            { name = "2017", expanded = True, photoUrls = []
+            , subfolders =
+              [ Folder
+                { name = "outdoors"
+                , expanded = True, photoUrls = [], subfolders = []
+                }
+              , Folder
+                { name = "indoors"
+                , expanded = True, photoUrls = [], subfolders = []
+                }
+              ]
+            }
+          ]
+        }
     }
 
 type Msg
   = ClickedPhoto String
   | GotInitialModel (Result Http.Error Model)
+  | ClickedFolder FolderPath
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
+    ClickedFolder path ->
+      ( { model | root = toggleExpanded path model.root }, Cmd.none )
     ClickedPhoto url ->
       ( { model | selectedPhotoUrl = Just url }, Cmd.none )
     GotInitialModel (Ok newModel) ->
@@ -92,7 +162,12 @@ view model =
           text ""
   in
   div [ class "content" ]
-      [ div [ class "selected-photo" ] [ selectedPhoto ] ]
+      [ div [ class "folders" ]
+        [ h1 [] [ text "Folders" ]
+        , viewFolder End model.root
+        ]
+      , div [ class "selected-photo" ] [ selectedPhoto ]
+      ]
 
 main : Program () Model Msg
 main =
@@ -130,6 +205,37 @@ viewRelatedPhoto url =
     , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
     ]
     []
+
+viewFolder : FolderPath -> Folder -> Html Msg
+viewFolder path (Folder folder) =
+  let
+    viewSubfolder : Int -> Folder -> Html Msg
+    viewSubfolder index subfolder =
+      viewFolder (appendIndex index path) subfolder
+
+    folderLabel =
+      label [ onClick (ClickedFolder path) ] [ text folder.name ]
+  in
+  if folder.expanded then
+    let
+      contents =
+        List.indexedMap viewSubfolder folder.subfolders
+    in
+    div [ class "folder expanded" ]
+        [ folderLabel
+        , div [ class "contents" ] contents
+        ]
+  else
+    div [ class "folder collapsed" ] [ folderLabel ]
+
+appendIndex : Int -> FolderPath -> FolderPath
+appendIndex index path =
+  case path of
+    End ->
+      Subfolder index End
+    
+    Subfolder subfolderIndex remainingPath ->
+      Subfolder subfolderIndex (appendIndex index remainingPath)
 
 urlPrefix : String
 urlPrefix =
