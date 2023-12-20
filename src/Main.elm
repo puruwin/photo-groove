8b8a8b0cff861888fd37add0fe69cea346580361
puruwin <taborlindp@gmail.com>
2023-12-20 16:21:14 +0100

    handling URL changes

--- src/Main.elm
+++ src/Main.elm
@@ -9,7 +9,7 @@ import Html.Lazy exposing (lazy)
 import Html.Attributes exposing (classList, href)
 
 type alias Model =
-  { page : Page }
+  { page : Page, key : Nav.Key }
 
 type Page
   = SelectedPhoto String
@@ -66,11 +66,20 @@ viewFooter =
   footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
 
 type Msg
-  = NothingYet
+  = ClickedLink Browser.UrlRequest
+  | ChangedUrl Url
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  ( model, Cmd.none )
+  case msg of
+    ClickedLink urlRequest ->
+      case urlRequest of
+        Browser.External href ->
+          ( model, Nav.load href )
+        Browser.Internal url ->
+          ( model, Nav.pushUrl model.key (Url.toString url) )
+    ChangedUrl url ->
+      ( { model | page = urlToPage url }, Cmd.none )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -78,7 +87,7 @@ subscriptions model =
 
 init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( { page = urlToPage url }, Cmd.none )
+  ( { page = urlToPage url, key = key }, Cmd.none )
 
 urlToPage : Url -> Page
 urlToPage url =
@@ -97,8 +106,8 @@ main : Program () Model Msg
 main =
   Browser.application
     { init = init
-    , onUrlRequest = \_ -> Debug.todo "handle URL requests"
-    , onUrlChange = \_ -> Debug.todo "handle URL requests"
+    , onUrlRequest = ClickedLink
+    , onUrlChange = ChangedUrl
     , subscriptions = \_ -> Sub.none
     , update = update
     , view = view
